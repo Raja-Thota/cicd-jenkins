38dcfd36459779cd7090b42f4a7856657c7f39ff
Raja-Thota <rajasaikiranthota989@gmail.com>
2024-11-16 15:45:06 -0500

    added install and jenkins file

---
+++ install.sh
@@ -0,0 +1,33 @@
+sudo apt update
+sudo apt upgrade -y
+
+#Install jenkins
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt install openjdk-17-jdk -y
+sudo apt-get install jenkins
+sudo systemctl enables jenkins
+sudo systemctl status jenkins
+
+#Docker installation
+curl https://get.docker.com | bash
+sudo usermod -aG docker jenkins
+newgrp docker
+sudo systemctl enable docker
+#######################
+sudo systemctl stop jenkins
+sudo systemctl start jenkins
+
+######Start Nexus as Docker Container################
+docker run -d --name nexus -p 8081:8081 -v /home/ubuntu/nexus:/nexus-data sonatype/nexus3:latest
+
+######Start SonarQube as Docker Container################
+
+docker run -d --name sonar -p 9000:9000 -v /home/ubuntu/sonar:/opt/sonarqube/data \
+                                        -v /home/ubuntu/sonar/extension:/opt/sonarqube/extension \
+                                        -v /home/ubuntu/sonar/logs:/opt/sonarqube/logs \
+                                        sonarqube:lts-community
